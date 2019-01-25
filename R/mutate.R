c5d02c9773363525e6634fefb845d0b8f6098a0b
Benjamin Elbers <elbersb@gmail.com>
2019-01-25 18:31:46 +1100

    transmute: add support

--- R/mutate.R
+++ R/mutate.R
@@ -28,11 +28,53 @@ mutate_at <- function(.data, ...) {
     log_mutate(.data, dplyr::mutate_at, "mutate_at", ...)
 }
 
+#' Wrapper around dplyr::transmute and related functions
+#' that prints information about the operation
+#'
+#' @param .data a tbl; see \link[dplyr]{transmute}
+#' @param ... see \link[dplyr]{transmute}
+#' @return see \link[dplyr]{transmute}
+#' @import dplyr
+#' @export
+transmute <- function(.data, ...) {
+    log_mutate(.data, dplyr::transmute, "transmute", ...)
+}
+
+#' @rdname transmute
+#' @export
+transmute_all <- function(.data, ...) {
+    log_mutate(.data, dplyr::transmute_all, "transmute_all", ...)
+}
+
+#' @rdname transmute
+#' @export
+transmute_if <- function(.data, ...) {
+    log_mutate(.data, dplyr::transmute_if, "transmute_if", ...)
+}
+
+#' @rdname transmute
+#' @export
+transmute_at <- function(.data, ...) {
+    log_mutate(.data, dplyr::transmute_at, "transmute_at", ...)
+}
+
 log_mutate <- function(.data, fun, funname, ...) {
     cols <- names(.data)
     newdata <- fun(.data, ...)
     if (!"data.frame" %in% class(.data)) {
         return(newdata)
+    }
+
+    if (grepl("transmute", funname)) {
+        dropped_vars <- setdiff(names(.data), names(newdata))
+        n <- length(dropped_vars)
+        if (ncol(newdata) == 0) {
+            cat(glue::glue("{funname}: dropped all variables"), "\n")
+            return(newdata)
+        } else if (length(dropped_vars) > 0) {
+            cat(glue::glue("{funname}: dropped {plural(n, 'variable')}",
+                           " ({format_list(dropped_vars)})"), "\n")
+        }
     }
 
     has_changed <- FALSE
