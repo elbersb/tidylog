74fcf2868de5cb198de1a91890452d7c0d175122
Benjamin Elbers <elbersb@gmail.com>
2019-02-06 11:19:51 +1100

    join: use correct argument name (fixes #7)

--- R/join.R
+++ R/join.R
@@ -2,55 +2,55 @@
 #' that prints information about the operation
 #'
 #'
-#' @param .data a tbl; see \link[dplyr]{inner_join}
+#' @param x a tbl; see \link[dplyr]{inner_join}
 #' @param ... see \link[dplyr]{inner_join}
 #' @return see \link[dplyr]{inner_join}
 #' @import dplyr
 #' @export
-inner_join <- function(.data, ...) {
-    log_join(.data, dplyr::inner_join, "inner_join", ...)
+inner_join <- function(x, ...) {
+    log_join(x, dplyr::inner_join, "inner_join", ...)
 }
 
 #' @rdname inner_join
 #' @export
-full_join <- function(.data, ...) {
-    log_join(.data, dplyr::full_join, "full_join", ...)
+full_join <- function(x, ...) {
+    log_join(x, dplyr::full_join, "full_join", ...)
 }
 
 #' @rdname inner_join
 #' @export
-left_join <- function(.data, ...) {
-    log_join(.data, dplyr::left_join, "left_join", ...)
+left_join <- function(x, ...) {
+    log_join(x, dplyr::left_join, "left_join", ...)
 }
 
 #' @rdname inner_join
 #' @export
-right_join <- function(.data, ...) {
-    log_join(.data, dplyr::right_join, "right_join", ...)
+right_join <- function(x, ...) {
+    log_join(x, dplyr::right_join, "right_join", ...)
 }
 
 #' @rdname inner_join
 #' @export
-anti_join <- function(.data, ...) {
-    log_join(.data, dplyr::anti_join, "anti_join", ...)
+anti_join <- function(x, ...) {
+    log_join(x, dplyr::anti_join, "anti_join", ...)
 }
 
 #' @rdname inner_join
 #' @export
-semi_join <- function(.data, ...) {
-    log_join(.data, dplyr::semi_join, "semi_join", ...)
+semi_join <- function(x, ...) {
+    log_join(x, dplyr::semi_join, "semi_join", ...)
 }
 
-log_join <- function(.data, fun, funname, ...) {
-    newdata <- fun(.data, ...)
-    if (!"data.frame" %in% class(.data) | !should_display()) {
+log_join <- function(x, fun, funname, ...) {
+    newdata <- fun(x, ...)
+    if (!"data.frame" %in% class(x) | !should_display()) {
         return(newdata)
     }
-    n_rows <- nrow(newdata) - nrow(.data)
+    n_rows <- nrow(newdata) - nrow(x)
     text_rows <- ifelse(n_rows >= 0, "added", "removed")
     n_rows <- abs(n_rows)
 
-    cols <- setdiff(names(newdata), names(.data))
+    cols <- setdiff(names(newdata), names(x))
 
     if (length(cols) == 0) {
         display(glue::glue("{funname}: {text_rows} {plural(n_rows, 'row')} and ",
