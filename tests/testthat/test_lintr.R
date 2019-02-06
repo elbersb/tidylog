dc87cc4d3ab488142887aa86c62f210db07d7642
Benjamin Elbers <elbersb@gmail.com>
2019-02-06 12:50:28 +1100

    update lintr

--- tests/testthat/test_lintr.R
+++ tests/testthat/test_lintr.R
@@ -2,6 +2,10 @@ if (requireNamespace("lintr", quietly = TRUE)) {
     context("lintr")
 
     test_that("Package Style", {
-        lintr::expect_lint_free()
+
+        lintr::expect_lint_free(linters = lintr::with_defaults(
+            object_usage_linter = NULL,
+            object_name_linter = NULL,
+            line_length_linter = lintr::line_length_linter(100)))
     })
 }
