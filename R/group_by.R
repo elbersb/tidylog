9c1390a3d998564943f20f4c35e33a3b08563b4b
damianooldoni <damiano.oldoni@inbo.be>
2019-10-31 15:16:45 +0100

    Add ungropp to group_by and do the same with tests

--- R/group_by.R
+++ R/group_by.R
@@ -5,8 +5,10 @@
 #' @param ... see \link[dplyr]{group_by}
 #' @return see \link[dplyr]{group_by}
 #' @examples
-#' group_by(mtcars, am, cyl)
+#' mtcars <- group_by(mtcars, am, cyl)
 #' #> group_by: 2 grouping variables (am, cyl)
+#' mtcars <- ungroup(mtcars)
+#' #> ungroup: no grouping variables left
 #' @import dplyr
 #' @export
 group_by <- function(.data, ...) {
@@ -40,5 +42,22 @@ log_group_by <- function(.data, .fun, .funname, ...) {
     display(glue::glue(
         "{.funname}: {plural(length(group_vars), 'grouping variable')} ",
         "({format_list(group_vars)})"))
+    newdata
+}
+
+#' @rdname group_by
+#' @export
+ungroup <- function(.data, ...) {
+    log_ungroup(.data, .fun = dplyr::ungroup, .funname = "ungroup", ...)
+}
+
+
+log_ungroup <- function(.data, .fun, .funname, ...) {
+    newdata <- .fun(.data, ...)
+    if (!"data.frame" %in% class(.data) | !should_display()) {
+        return(newdata)
+    }
+    display(glue::glue(
+        "{.funname}: no grouping variables left"))
     newdata
 }
--- tests/testthat/test_group_by.R
+++ tests/testthat/test_group_by.R
@@ -31,3 +31,14 @@ test_that("group_by: argument order", {
     })
     expect_equal(is.grouped_df(out), TRUE)
 })
+
+test_that("ungroup", {
+    expect_message({
+        out <- tidylog::ungroup(mtcars, mpg)
+    })
+    expect_equal(dplyr::is.grouped_df(out), FALSE)
+
+    expect_silent({
+        out <- dplyr::ungroup(mtcars, mpg)
+    })
+})
