3e63e9dee7cc09b4fff5105673a7538f1aa6ae18
orgadish <48453207+orgadish@users.noreply.github.com>
2023-09-24 22:25:50 -0700

    Refactor test_longer_wider to be easier to extend.

--- tests/testthat/test_longer_wider.R
+++ tests/testthat/test_longer_wider.R
@@ -3,115 +3,79 @@ library("tidyr")
 library("tidylog")
 context("test_longer_wider")
 
-test_that("pivot_longer", {
-    mtcars$id <- seq_len(nrow(mtcars))
+mtcars_with_id <- dplyr::mutate(mtcars, id = seq_len(dplyr::n()))
+mtcars_with_name <- tidyr::as_tibble(mtcars, rownames = "name")
 
-    expect_message({
-        outlog <- tidylog::pivot_longer(mtcars, -id, names_to = "var", values_to = "value")
+test_that_tidylog_matches_tidyr <- function(fun_name, tidylog_fun, tidyr_fun, ..., expected_nrow, expected_ncol) {
+    test_that(fun_name, {
+        expect_message({
+            outlog <- tidylog_fun(...)
+        })
+        expect_equal(nrow(outlog), expected_nrow)
+        expect_equal(ncol(outlog), expected_ncol)
+
+        expect_silent({
+            outtidy <- tidyr_fun(...)
+        })
+
+        expect_equal(outlog, outtidy)
     })
-    expect_equal(nrow(outlog), 352)
-    expect_equal(ncol(outlog), 3)
+}
 
-    expect_silent({
-        outtidy <- tidyr::pivot_longer(mtcars, -id, names_to = "var", values_to = "value")
+test_argument_order <- function(fun_name, tidylog_fun, data, ...) {
+    test_that(glue::glue("{fun_name}: argument order"), {
+        expect_message({
+            out_ab <- tidylog_fun(data = data, ...)
+        })
+
+        expect_message({
+            out_ba <- tidylog_fun(..., data = data)
+        })
+
+        expect_equal(out_ab, out_ba)
     })
+}
 
-    expect_equal(outlog, outtidy)
+test_that_tidylog_matches_tidyr(
+    "pivot_longer", tidylog::pivot_longer, tidyr::pivot_longer,
+    mtcars_with_id, -id, names_to = "var", values_to = "value",
+    expected_nrow = 352, expected_ncol = 3
+)
 
-})
+test_argument_order(
+    "pivot_longer", tidylog::pivot_longer,
+    data = mtcars_with_id, cols = -id, names_to = "var", values_to = "value"
+)
 
-test_that("pivot_longer: argument order", {
-    mtcars$id <- seq_len(nrow(mtcars))
+test_that_tidylog_matches_tidyr(
+    "pivot_wider", tidylog::pivot_wider, tidyr::pivot_wider,
+    mtcars, names_from = vs, values_from = cyl,
+    expected_nrow = 32, expected_ncol = 11
+)
 
-    expect_message({
-        out_ab <- tidylog::pivot_longer(names_to = "var", values_to = "value",
-            data = mtcars, cols = -id)
-    })
+test_argument_order(
+    "pivot_wider", tidylog::pivot_wider,
+    mtcars, names_from = vs, values_from = cyl
+)
 
-    expect_message({
-        out_ba <- tidylog::pivot_longer(data = mtcars, cols = -id,
-            names_to = "var", values_to = "value")
-    })
+test_that_tidylog_matches_tidyr(
+    "gather", tidylog::gather, tidyr::gather,
+    mtcars,
+    expected_nrow = 352, expected_ncol = 2
+)
 
-    expect_equal(out_ab, out_ba)
-})
+test_argument_order(
+    "gather", tidylog::gather,
+    mtcars, carb
+)
 
-test_that("pivot_wider", {
-    expect_message({
-        outlog <- tidylog::pivot_wider(mtcars, names_from = vs, values_from = cyl)
-    })
-    expect_equal(nrow(outlog), 32)
-    expect_equal(ncol(outlog), 11)
+test_that_tidylog_matches_tidyr(
+    "spread", tidylog::spread, tidyr::spread,
+    mtcars, hp, carb,
+    expected_nrow = 32, expected_ncol = 31
+)
 
-    expect_silent({
-        outtidyr <- tidyr::pivot_wider(mtcars, names_from = vs, values_from = cyl)
-    })
-
-    expect_equal(outlog, outtidyr)
-})
-
-test_that("pivot_wider: argument order", {
-    expect_message({
-        out_ab <- tidylog::pivot_wider(names_from = vs, values_from = cyl, data = mtcars)
-    })
-
-    expect_message({
-        out_ba <- tidylog::pivot_wider(mtcars, names_from = vs, values_from = cyl)
-    })
-
-    expect_equal(out_ab, out_ba)
-})
-
-test_that("gather", {
-    expect_message({
-        outlog <- tidylog::gather(mtcars)
-    })
-    expect_equal(nrow(outlog), 352)
-    expect_equal(ncol(outlog), 2)
-
-    expect_silent({
-        outtidy <- tidyr::gather(mtcars)
-    })
-
-    expect_equal(outlog, outtidy)
-
-})
-
-test_that("gather: argument order", {
-    expect_message({
-        out_ab <- tidylog::gather(data = mtcars, carb)
-    })
-
-    expect_message({
-        out_ba <- tidylog::gather(carb, data = mtcars)
-    })
-
-    expect_equal(out_ab, out_ba)
-})
-
-test_that("spread", {
-    expect_message({
-        outlog <- tidylog::spread(mtcars, hp, carb)
-    })
-    expect_equal(nrow(outlog), 32)
-    expect_equal(ncol(outlog), 31)
-
-    expect_silent({
-        outtidyr <- tidyr::spread(mtcars, hp, carb)
-    })
-
-    expect_equal(outlog, outtidyr)
-
-})
-
-test_that("spread: argument order", {
-    expect_message({
-        out_ab <- tidylog::spread(data = mtcars, hp, carb)
-    })
-
-    expect_message({
-        out_ba <- tidylog::spread(hp, carb, data = mtcars)
-    })
-
-    expect_equal(out_ab, out_ba)
-})
+test_argument_order(
+    "spread", tidylog::spread,
+    mtcars, hp, carb
+)
