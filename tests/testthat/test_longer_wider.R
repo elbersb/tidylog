2a752edb492010063430fae6097a6d87f4a0c9fb
orgadish <48453207+orgadish@users.noreply.github.com>
2023-09-24 22:29:13 -0700

    Modify refactor further to combine matching and argument order test into a single function for each.

--- tests/testthat/test_longer_wider.R
+++ tests/testthat/test_longer_wider.R
@@ -6,23 +6,23 @@ context("test_longer_wider")
 mtcars_with_id <- dplyr::mutate(mtcars, id = seq_len(dplyr::n()))
 mtcars_with_name <- tidyr::as_tibble(mtcars, rownames = "name")
 
-test_that_tidylog_matches_tidyr <- function(fun_name, tidylog_fun, tidyr_fun, ..., expected_nrow, expected_ncol) {
+test_tidyr_fun <- function(fun_name, tidylog_fun, tidyr_fun, data, ..., expected_nrow, expected_ncol) {
+    # Test that tidylog output matches tidyr.
     test_that(fun_name, {
         expect_message({
-            outlog <- tidylog_fun(...)
+            outlog <- tidylog_fun(data, ...)
         })
         expect_equal(nrow(outlog), expected_nrow)
         expect_equal(ncol(outlog), expected_ncol)
 
         expect_silent({
-            outtidy <- tidyr_fun(...)
+            outtidy <- tidyr_fun(data, ...)
         })
 
         expect_equal(outlog, outtidy)
     })
-}
 
-test_argument_order <- function(fun_name, tidylog_fun, data, ...) {
+    # Test that argument order doesn't matter.
     test_that(glue::glue("{fun_name}: argument order"), {
         expect_message({
             out_ab <- tidylog_fun(data = data, ...)
@@ -36,46 +36,28 @@ test_argument_order <- function(fun_name, tidylog_fun, data, ...) {
     })
 }
 
-test_that_tidylog_matches_tidyr(
+
+test_tidyr_fun(
     "pivot_longer", tidylog::pivot_longer, tidyr::pivot_longer,
     mtcars_with_id, -id, names_to = "var", values_to = "value",
     expected_nrow = 352, expected_ncol = 3
 )
 
-test_argument_order(
-    "pivot_longer", tidylog::pivot_longer,
-    data = mtcars_with_id, cols = -id, names_to = "var", values_to = "value"
-)
 
-test_that_tidylog_matches_tidyr(
+test_tidyr_fun(
     "pivot_wider", tidylog::pivot_wider, tidyr::pivot_wider,
     mtcars, names_from = vs, values_from = cyl,
     expected_nrow = 32, expected_ncol = 11
 )
 
-test_argument_order(
-    "pivot_wider", tidylog::pivot_wider,
-    mtcars, names_from = vs, values_from = cyl
-)
-
-test_that_tidylog_matches_tidyr(
+test_tidyr_fun(
     "gather", tidylog::gather, tidyr::gather,
     mtcars,
     expected_nrow = 352, expected_ncol = 2
 )
 
-test_argument_order(
-    "gather", tidylog::gather,
-    mtcars, carb
-)
-
-test_that_tidylog_matches_tidyr(
+test_tidyr_fun(
     "spread", tidylog::spread, tidyr::spread,
     mtcars, hp, carb,
     expected_nrow = 32, expected_ncol = 31
-)
-
-test_argument_order(
-    "spread", tidylog::spread,
-    mtcars, hp, carb
 )
