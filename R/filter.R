60044d329d9220c080d0ce8af11e3e813ea5edb2
orgadish <48453207+orgadish@users.noreply.github.com>
2026-06-18 21:18:03 -0700

    Revert "Move should_display check out of display_changed_rows"
    This reverts commit d4fcfd26e421398d9fb8c7c5e6d17d9e9e10920f.

--- R/filter.R
+++ R/filter.R
@@ -47,11 +47,6 @@ display_slice_ties <- function(.olddata, .newdata, .funname, ...) {
 }
 
 log_filter <- function(.olddata, .newdata, .funname, ...) {
-    if (!"data.frame" %in% class(.olddata)
-        | !"data.frame" %in% class(.newdata)
-        | !should_display()) {
-        return()
-    }
     display_changed_rows(.olddata, .newdata, .funname)
 
     if (.funname %in% c("slice_min", "slice_max")) {
